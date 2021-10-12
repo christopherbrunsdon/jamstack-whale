1c2f24360a2ff53e47d7ed56f725593f2cf1ea5f
Christopher Brunsdon <christopher.brunsdon@onedayonly.co.za>
2021-10-12 22:48:12 +0200

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,19 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+// import { StaticImage } from "gatsby-plugin-image"
 
+import whaleGIF from "../images/gifs/whales.gif"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <h1>Adopt a Whale</h1>
+    <p>Whales are our friends.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <img src={whaleGIF} alt="Picture of Whale from BBC America" />
+    </div>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
